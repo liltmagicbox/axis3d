4c74e255d425ec1075b648ddabb74046e1fb8f07
liltmagicbox <68154458+liltmagicbox@users.noreply.github.com>
2023-09-02 23:08:29 +0900

    Great UnitFactory Created!
    UnitFactory,
    set name, def.attr_dict , behavior
    (for batched update)
    and array. (if not, fast small UnitDict.)
    
    order by name, n, whole(optional)
    if whole, it returns individual array used.

--- gunfire_unit.py
+++ gunfire_unit.py
@@ -13,8 +13,8 @@ if not platform.architecture()[0] == '64bit':
 	DTYPE_INT = 'int32'
 
 
-
-
+def make_idxs(arr):
+	return np.nonzero(arr)[0]
 def make_rand(n):
 	return np.random.rand(n)
 def make_randn(n):
@@ -33,11 +33,11 @@ def make_1d(n):
 	return np.zeros(n).astype(DTYPE_FLOAT)
 def make_nd(n,rows):
 	return np.zeros( (rows, n) ).astype(DTYPE_FLOAT)
-def make_arr(value):
+def make_arr(n, value):
 	try:
-		return make_nd(DEFAULT_ARR_LENGTH, len(value) )
+		return make_nd(n, len(value) )
 	except:
-		return make_1d(DEFAULT_ARR_LENGTH)
+		return make_1d(n)
 #----
 def extend_1d(arr, n=0):
 	old_n = len(arr)
@@ -103,24 +103,24 @@ def parse_xyz(key):
 #=========================
 
 class UnitArray:
-	def __init__(self, **kwargs):
-		if not kwargs: raise KeyError("key:value is required to init UnitArray.")
-		self.default = {}
+	def __init__(self, attr_dict, n=None):
+		n = DEFAULT_ARR_LENGTH if n is None else n
+		self.fixed = False if n == DEFAULT_ARR_LENGTH else True
+		self.default = attr_dict.copy()
+		
 		self.free_idxs = []
 		self.active = None
 		self.data = {}
-		self._setdefault(kwargs)
-	def _setdefault(self, kwargs):
-		self.default = kwargs.copy()
-		self._setdata()
-	def _setdata(self):
-		self.free_idxs = [i for i in range(DEFAULT_ARR_LENGTH)]
-		self.active = make_b(DEFAULT_ARR_LENGTH)
+		self._setdata(n)
+	def _setdata(self, n):
+		self.free_idxs = [i for i in range(n)]
+		self.active = make_b(n)
 		self.data = {}
 		for key, value in self.default.items():
-			self.data[key] = make_arr(value)
+			self.data[key] = make_arr(n, value)
 	#--------------
 	def __len__(self):
+		"for internal only.. like rand."
 		return len(tuple(self.data.values())[0])
 	
 	def __repr__(self):
@@ -135,7 +135,7 @@ class UnitArray:
 		now_n = len(self)
 		half_n = now_n//2
 		ext_n = half_n if n<half_n else n
-		if now_n+ext_n > MAX_ARR_LENGTH: raise OverflowError("array exceeds max len:"+str(MAX_ARR_LENGTH))
+		if (now_n+ext_n > MAX_ARR_LENGTH) or self.fixed: raise OverflowError("array can't extend")
 		#---
 		for key, arr in self.data.items():
 			self.data[key] = extend_arr(arr, ext_n)
@@ -154,6 +154,7 @@ class UnitArray:
 		self.free_idxs = self.free_idxs[:-n]
 		return make_i(idxs)
 	#-----	
+	#external 4 interfaces.
 	def acquire(self, n=1, **kwargs):
 		if n<1:raise ValueError("n must be >=1")
 		idxs = self._get_free_idxs(n)
@@ -165,7 +166,6 @@ class UnitArray:
 	def release(self, idxs):
 		self.free_idxs.extend(idxs)
 		self.active[idxs] = False
-
 	def set(self, key,value, idxs=None):
 		try:
 			return set_arr(self.data[key], value, idxs)
@@ -179,6 +179,13 @@ class UnitArray:
 		except:
 			key,i = parse_xyz(key)
 			return get_arr(self.data[key][i], idxs)
+	#-----
+	#if self.fixed
+	def get_active_idxs(self):
+		"1M, 2.5ms"
+		return make_idxs(self.active)
+	#-----
+	#tiny randoms
 	def rand(self):
 		return make_rand(len(self))
 	def randn(self):
@@ -187,44 +194,49 @@ class UnitArray:
 		return make_randint(len(self), a,b)
 
 
-#how 7,7,7 to 3,1?
-ua = UnitArray(hp=10,mp=5,pos=(7,7,7))
-print(ua.default)
 
-ua.set('posx', 3)
-print(ua.get('posx') )
-ua.set('posx', [6,7,8,9,5])
-print(ua.get('posx') )
-ua.set('hp' ,ua.randint(1,55) )
-print(ua)
-x = ua.acquire(hp=99)
-x = ua.acquire(2)
-x = ua.acquire(3, mp=[11,12,13])
-x = ua.acquire(2, pos=(3,4,3) )
-x = ua.acquire(2, pos=([11,22],[33,44],[55,66]) )
-ua.release([3])
-ua.release([1,5])
-print(x)
-print(ua)
-#print(ua.active)
+def main():
+	#how 7,7,7 to 3,1?
+	dd = {'hp':10,'mp':5,'pos':(7,7,7)}
+	ua = UnitArray( dd)
+	print(ua.default)
 
-ua = UnitArray(hp=10,mp=5,pos=(7,7,7))
-ua.set('hp',[5])
-ua.set('mp',[2])
-ua.set('hp',3)
-ua.set('mp',4)
-ua.set('hp',[5],[2])
-ua.set('mp',[2],[2])
-ua.set('hp',3,[2])
-ua.set('mp',4,[2])
-#us.set('posx', [2] )
+	ua.set('posx', 3)
+	print(ua.get('posx') )
+	ua.set('posx', [6,7,8,9,5])
+	print(ua.get('posx') )
+	ua.set('hp' ,ua.randint(1,55) )
+	print(ua)
+	x = ua.acquire(hp=99)
+	x = ua.acquire(2)
+	x = ua.acquire(3, mp=[11,12,13])
+	x = ua.acquire(2, pos=(3,4,3) )
+	x = ua.acquire(2, pos=([11,22],[33,44],[55,66]) )
+	ua.release([3])
+	ua.release([1,5])
+	print(x)
+	print(ua)
+	#print(ua.active)
 
-ua.set('pos',(1,2,3) )
-ua.set('pos',([1],[2],[3]))
-ua.set('pos',([11],[22],[33]) ,[3])
-print(ua)
+	ua = UnitArray(dd)
+	ua.set('hp',[5])
+	ua.set('mp',[2])
+	ua.set('hp',3)
+	ua.set('mp',4)
+	ua.set('hp',[5],[2])
+	ua.set('mp',[2],[2])
+	ua.set('hp',3,[2])
+	ua.set('mp',4,[2])
+	#us.set('posx', [2] )
 
-ua.set('hp',[1,2,3,4,5])
-print(ua.get('hp',[1,3,4]) )
+	ua.set('pos',(1,2,3) )
+	ua.set('pos',([1],[2],[3]))
+	ua.set('pos',([11],[22],[33]) ,[3])
+	print(ua)
+
+	ua.set('hp',[1,2,3,4,5])
+	print(ua.get('hp',[1,3,4]) )
 
 
+if __name__ == '__main__':
+	main()
---
+++ unitfactory.py
@@ -0,0 +1,146 @@
+from gunfire_unit import UnitArray
+
+
+class UnitDict:
+	def __init__(self, attr_dict, n=1):
+		self.default = attr_dict.copy()
+		self.data = {}
+		self.counter = 0
+		idxs = self.acquire(n)
+	def set(self, key,value, idxs=None):
+		if idxs is None:
+			idxs = self.data.keys()
+
+		try:
+			if len(value) == len(idxs):
+				for i, idx in enumerate(idxs):
+					self.data[idx][key] = value[i]
+			else:
+				raise ValueError(f'value len not matching: {len(value)}, expected:{len(idxs)}')
+		except TypeError:
+			for idx in idxs:
+				self.data[idx][key] = value
+
+	def get(self, key, idxs=None):
+		if idxs is None:
+			idxs = self.data.keys()
+		return [ self.data[idx][key] for idx in idxs]
+
+	def acquire(self, n=1, **kwargs):
+		if n == 1:
+			attr_dict = self.default.copy()
+			attr_dict.update(kwargs)
+			self.counter+=1
+			self.data[self.counter] = attr_dict
+			return [self.counter]
+		else:
+			attr_dict = self.default.copy()
+			long_dict = {}
+			for key,value in kwargs.items():
+				try:
+					if len(value) == n:
+						long_dict[key] = value
+				except TypeError:
+					attr_dict[key] = value
+			#================
+			idxs = []
+			for i in range(n):
+				new_dict = attr_dict.copy()
+				for key,value in long_dict.items():
+					new_dict[key] = value[i]
+				self.counter+=1
+				self.data[self.counter] = new_dict
+				idxs.append(self.counter)
+			return idxs
+
+	def release(self, idxs):
+		for idx in idxs:
+			self.data.pop(idx)
+
+	def __len__(self):
+		return len(self.data)
+	def rand(self):
+		return [random.random() for i in range(len(self))]
+	def randn(self):
+		return [random.randn() for i in range(len(self))]
+	def randint(self, a,b=None):
+		return [random.randint(a,b) for i in range(len(self))]
+
+
+
+#===================================
+#attrs?
+class Unit:
+	def __init__(self, unit_array, idxs=None, behavior=None ):
+		object.__setattr__(self, '_data', unit_array)
+		object.__setattr__(self, '_idxs', idxs)
+		object.__setattr__(self, 'behavior', behavior)
+		object.__setattr__(self, 'attrs', tuple(unit_array.default.keys()) )
+	def __setattr__(self, key,value):
+		self._data.set(key,value, self._idxs)
+	def __getattr__(self, key):
+		return self._data.get(key, self._idxs)
+	def __del__(self):
+		if self._idxs:
+			self._data.release(self._idxs)
+	#----
+	def rand(self):
+		return self._data.rand()
+	def randn(self):
+		return self._data.randn()
+	def randint(self, a,b=None):
+		return self._data.randint(a,b)
+	#====
+	def execute(self, function, *args, **kwargs):
+		function(self, *args, **kwargs)
+	def update(self,dt):
+		if self.behavior is None:return
+		for func in self.behavior:
+			self.execute(func,dt)
+
+class UnitFactory:
+	def __init__(self):
+		self.data[name] = {}
+		self.behavior[name] = {}
+		self.units[name] = {}
+	
+	def set(self, name, data, behavior=None, array = True):
+		self.data[name] = data
+		self.behavior[name] = behavior
+		if array:
+			self.units[name] = UnitArray(data)
+		else:
+			self.units[name] = UnitDict(data)
+
+	def order(self, name, n=1, whole=False):
+		if whole:
+			ua = UnitArray( self.data[name] , n)
+			return Unit(ua, behavior = self.behavior[name])  #update itself!
+		ua = self.units[name]
+		idxs = ua.acquire(n, **kwargs)
+		return Unit(ua,idxs)
+
+	def update(self,dt):
+		for name, be in self.behavior.items():
+			for func in be:
+				units = self.units[name]
+				units.execute(func,dt)
+
+
+
+
+
+
+
+#========================================
+def minitest():
+	a = Unit( UnitDict({'pos':3},7) )
+	a = Unit( UnitArray({'pos':3},7) )
+	a.pos=[i for i in range(7)]
+	print(a.pos)
+
+def main():
+	minitest()
+
+if __name__ == '__main__':
+	main()
